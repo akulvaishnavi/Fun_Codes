e4fcd19f3396feab827f31393ca5ea812661c789
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-07-10 19:20:25 +0530

    Update CircleArea.f90

--- CircleArea.f90
+++ CircleArea.f90
@@ -3,14 +3,11 @@ PROGRAM CircleArea
   REAL :: radius, area
   REAL, PARAMETER :: pi = 3.14159
 
-  ! Prompt the user to enter the radius of the circle
   PRINT *, 'Enter the radius of the circle:'
   READ *, radius
 
-  ! Calculate the area of the circle
   area = pi * radius * radius
 
-  ! Display the result
   PRINT *, 'The area of the circle is:', area
 
 END PROGRAM CircleArea
